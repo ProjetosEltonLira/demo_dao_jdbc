8531662f70d54e6e80659513cb193a88c344630f
ProjetosEltonLira <eltondelira@gmail.com>
2024-03-14 22:17:36 -0300

    Seller entitie class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args){
 
-        Department departamento = new Department(1,"Livros");
-        System.out.println(departamento);
+        Department departament = new Department(1,"Livros");
+        Seller seller = new Seller(1,"Bob","Bob@lee.com.br",new Date(),3000.00,departament);
+        System.out.println(seller);
     }
 }
