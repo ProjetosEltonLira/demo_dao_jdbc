249ab232da47a471251364e04b80da3c2ff32905
ProjetosEltonLira <eltondelira@gmail.com>
2024-03-15 19:01:25 -0300

    findByDepartment Implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -6,20 +6,27 @@ import model.dao.implementacao.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args){
 
-        //Department departament = new Department(1,"Livros");
-       //Seller seller = new Seller(1,"Bob","Bob@lee.com.br",new Date(),3000.00,departament);
+        //Department departament = new Department(2,null);
+        Department departament = new Department(2,"Livraria");
+        //Seller seller = new Seller(1,"Bob","Bob@lee.com.br",new Date(),3000.00,departament);
 
-        System.out.println("=== TESTE 1 : seller findById ===");
+
         //SellerDao sellerDao = new SellerDaoJDBC(Database.getConexao()); //Funciona também
         SellerDao sellerDao = DaoFactory.createSellerDAO(); //retorna a conexão com o banco de dados
 
+        System.out.println("=== TESTE 1 : seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("=== TESTE 2 : seller findByDepartment ===");
+        List<Seller> list = sellerDao.findByDepartment(departament);
+        list.forEach(System.out::println);
     }
 }
 
--- src/main/java/model/dao/implementacao/SellerDaoJDBC.java
+++ src/main/java/model/dao/implementacao/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -75,11 +78,54 @@ public class SellerDaoJDBC implements SellerDao {
 
     private static Department instanciarDepartamento(ResultSet result) throws SQLException {
         return new Department(result.getInt("DepartmentId"),
-                result.getString("DepName"));
+                              result.getString("DepName"));
     }
 
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = conexao.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    +"FROM seller INNER JOIN department "
+                    +"ON seller.DepartmentId = department.Id "
+                    +"WHERE DepartmentId = ? "
+                    +"ORDER BY Name");
+            statement.setInt(1, department.getId());
+            resultSet = statement.executeQuery();
+
+
+            List<Seller> sellerList = new ArrayList<>();
+            //Solução um Dificil.
+
+            Map<Integer, Department> map = new HashMap<>();
+            while (resultSet.next()) {
+                Department dep = map.get(resultSet.getInt("DepartmentId"));//Existe algum departamento com esse id.
+                if (dep == null) {
+                    dep = instanciarDepartamento(resultSet);
+                    map.put(dep.getId(), dep);
+                }
+                Seller seller = instanciarSeller(resultSet, department);
+                sellerList.add(seller);
+
+            }
+            /*Solução simples #2
+            while(resultSet.next()) {
+                Seller seller = instanciarSeller(resultSet, department);
+                sellerList.add(seller);
+            }*/
+            return sellerList;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            Database.closeStatement(statement);
+            Database.closeResultSet(resultSet);
+        }
     }
 }
